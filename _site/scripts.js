847109549e9ca53d1adda46935dea63bfe45a6a9
chandlerdives <61131532+chandlerdives@users.noreply.github.com>
2020-08-17 17:00:32 +1000

    Initial Commit

---
+++ _site/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $(".hamburger").click(function(){
+    $(this).toggleClass('active');
+    $(".mobile-nav").fadeToggle();
+  });
+
+  $(document).ready(function(){
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is-active");
+  });
+});
+
+
+});
