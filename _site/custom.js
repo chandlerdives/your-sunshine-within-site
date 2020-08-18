7b08ac9d6a2b249c5dd72016fd1a7faa3c124173
chandlerdives <61131532+chandlerdives@users.noreply.github.com>
2020-08-18 21:49:43 +1000

    Testimonials banner!

---
+++ _site/custom.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  $('.quotes').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    speed: 800,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+
+
+$( document ).ready(function() {
+$('.no-fouc').removeClass('no-fouc');
+});
--- _site/scripts.js
+++ _site/scripts.js
@@ -11,5 +11,16 @@ $(document).ready(function(){
   });
 });
 
+  $('.quotes').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    speed: 800,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+
 
 });
